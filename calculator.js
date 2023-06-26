947fdde6079d14c8b2cf7d8331e3a04c92f6e2aa
Jhy-An Chen <jhyan.chen1@ucalgary.ca>
2023-06-26 06:36:49 -0700

    Implement initial version of calculator

---
+++ calculator.js
@@ -0,0 +1,138 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  if (num2 === 0) {
+    return "INF";
+  }
+  return num1 / num2;
+}
+
+function operate(operator, operand1, operand2) {
+  switch (operator) {
+    case "+":
+      return add(operand1, operand2);
+
+    case "-":
+      return subtract(operand1, operand2);
+
+    case "*":
+      return multiply(operand1, operand2);
+
+    case "/":
+      return divide(operand1, operand2);
+
+    default:
+      console.error(
+        `operate(${operate}, ${operand1}, ${operand2}) is invalid.`
+      );
+      return NaN;
+  }
+}
+
+function updateDisplay() {
+  const operand1Display = document.querySelector(".display-firstoperand");
+  const operand2Display = document.querySelector(".display-secondoperand");
+  const operatorDisplay = document.querySelector(".display-operator");
+  operand1Display.textContent = operand1;
+  operand2Display.textContent = operand2;
+  operatorDisplay.textContent = operator;
+}
+
+function numberButtonPressed(pressed) {
+  if (!/^[0-9,\.]$/.test(pressed)) {
+    console.error(`Invalid button ${pressed} pressed.`);
+    return;
+  }
+
+  operand2 += pressed;
+
+  updateDisplay();
+}
+
+function operatorButtonPressed(pressed) {
+  if (!/^[+,\-,*,/]$/.test(pressed)) {
+    console.error(`Invalid button ${pressed} pressed.`);
+    return;
+  }
+
+  if (operand2 === "") {
+    operand2 = ans;
+  }
+  operator = pressed;
+  operand1 = operand2;
+  operand2 = "";
+
+  updateDisplay();
+}
+
+function clearHistory() {
+  const historyDiv = document.querySelector(".display-history");
+  historyDiv.replaceChildren("");
+}
+
+function clearWorkspace() {
+  operand1 = "";
+  operator = "";
+  operand2 = "";
+}
+
+function clearButtonPressed() {
+  clearWorkspace();
+  clearHistory();
+  updateDisplay();
+}
+
+function backButtonPressed() {
+  if (operator2 !== "") {
+    operand2 = operator2.slice(0, -1);
+  } else if (operator !== "") {
+    operator = "";
+    operand1 = operand2;
+    operand1 = "";
+  } else if (operand1 !== "") {
+    operand1 = operand1.slice(0, -1);
+  }
+  updateDisplay();
+}
+
+function addHistoryLine(operator, operand1, operand2, ans) {
+  const historyDiv = document.querySelector(".display-history");
+  const historyLine = document.createElement("div");
+  historyLine.textContent = `${operand1} ${operator} ${operand2} = ${ans}`;
+  historyLine.classList.add("display-history-line");
+  historyDiv.appendChild(historyLine);
+}
+
+function equalsButtonPressed() {
+  num1 = parseFloat(operand1);
+  num2 = parseFloat(operand2);
+
+  if (isNaN(num1) || isNaN(num2)) {
+    console.error("Invalid operands");
+    return;
+  }
+
+  ans = operate(operator, num1, num2);
+  if (isNaN(ans)) {
+    console.error("Invalid output");
+  } else {
+    addHistoryLine(operator, num1, num2, ans);
+  }
+  clearWorkspace();
+  updateDisplay();
+}
+
+let operand1 = "";
+let operator = "";
+let operand2 = "";
+let ans = "";
